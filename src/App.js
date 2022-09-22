21d98e20e5140e4f1455d90cecda3f8aaf6b5d1f
brain-official <sammyjoe498@gmail.com>
2022-09-22 18:39:53 +0100

    setting up instagram front page for mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header"
+import Home from "./Components/Home";
+import Search from "./Components/Search"
+import AddPost from './Components/AddPost'
+import Likes from './Components/Likes'
+import Profile from "./Components/Profile"
+import Footer from "./Components/Footer";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+
+        <Route path="/" element={<Search />}></Route>
+
+        <Route path="/" element={<AddPost />}></Route>
+        
+        <Route path="/" element={<Likes />}></Route>
+        
+        <Route path="/" element={<Profile/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
+
 export default App;
+
+
---
+++ src/Components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Header from './Header'
+// import Footer from './Footer'
+
+
+const Layout = () => {
+    return (
+        <div>Layout</div>
+    )
+}
+
+
+
+export default Layout
