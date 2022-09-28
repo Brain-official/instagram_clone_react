2d47c22789f98465ee1110cbf9290f51345d60dd
brain-official <sammyjoe498@gmail.com>
2022-09-28 20:57:04 +0100

    updated feeds components

--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from "./Layout/Layout.jsx"
+import StatusCircles from './StatusCircles.jsx'
+
 
 
 const Feeds = () => {
     return (
         <Layout>
+            <StatusCircles />
             <h1>lorem ipsum lorem ipsum lorem ikpsndviavdmmasidbhv zbxc zjxb  dz 
                 dbcxbghbvkzmbhjnjsn vxfgnnh lorem*
                 lorem ipsum lorem ipsum lorem ikpsndviavdmmasidbhv zbxc zjxb  dz 
@@ -61,3 +64,6 @@ const Feeds = () => {
 
 
 export default Feeds
+
+
+
